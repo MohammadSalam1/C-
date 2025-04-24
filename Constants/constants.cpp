5d5cb339383c043853af9b98676bf59aef5d3d5b
MohammadSalam1 <mhd.salam99@gmail.com>
2025-04-24 10:26:21 +0200

    constants are values that are unchangable

---
+++ Constants/constants.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+//const is a constant value that cannot be changed once it's set
+//exercises
+    /*
+     A1:
+     cont int max = 10;
+     //max = 20;
+     cout << max;
+     */
+    /*
+     void printVector(const vector<int>& v){
+         for (int i: v){
+         cout << i << " ";
+         }
+     }
+     in main
+     printVector(v);
+     cout << endl;
+     */
+
+    /*
+    int sumVector(const vector<int>& nums) {
+    int sum = 0;
+    vector<int>::const_iterator it;
+
+    for (it = nums.begin(); it != nums.end(); ++it) {
+        sum += *it;
+    }
+    return sum;
+}
+
+int main() {
+    vector<int> values = {1, 2, 3, 4};
+    cout << "Sum: " << sumVector(values) << endl;
+
+    return 0;
+}
+
+     */
+
+
+
+
+return 0;
+}
